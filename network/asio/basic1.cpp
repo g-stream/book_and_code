aaf7e3262d4c584c9e3de5cad5a14bc0db6c9212
yinly <yinly.chen@yandex.com>
2018-04-05 11:04:08 +0800

    basic asio

---
+++ network/asio/basic1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <boost/asio.hpp>
+int main()
+{
+    boost::asio::io_service io;
+
+    boost::asio::deadline_timer t(io, boost::posix_time::seconds(5));
+
+    t.wait();
+
+    std::cout << "Hello, world!\n";
+
+    return 0;
+}
