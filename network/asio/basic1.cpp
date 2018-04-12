bb6462629907a99e4dac7ad399bf6d1b48b170b5
yinly <yinly.chen@yandex.com>
2018-04-12 18:00:15 +0800

    add some example

--- network/asio/basic1.cpp
+++ network/asio/basic1.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <boost/asio.hpp>
+using boost::asio::ip::tcp;
 int main()
 {
     boost::asio::io_service io;
 
-    boost::asio::deadline_timer t(io, boost::posix_time::seconds(5));
+    boost::asio::deadline_timer t(io, boost::posix_time::milliseconds(10));
 
     t.wait();
+    tcp::resolver  resolver(io);
+    tcp::resolver::query query("www.yahoo.com", "80");
+    tcp::resolver::iterator iterator = resolver.resolve(query);
+    tcp::endpoint ep = *iterator;
+    std::cout << ep.address().to_string() << std::endl;
 
     std::cout << "Hello, world!\n";
 
